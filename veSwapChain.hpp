e289524be9ae3d4afc00b73d4721035b31380ce0
THermitwhy <18256253101@163.com>
2022-12-03 17:11:06 +0800

    up

--- veSwapChain.hpp
+++ veSwapChain.hpp
@@ -13,12 +13,13 @@ namespace vengin {
 		VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities);
 		void getSwapChainImages(VkDevice& device);
 		void createImageViews(VkDevice &device);
+		void createFrameBuffers();
 		void cleanImageViews(VkDevice& device);
 		//obj
 		VkSwapchainKHR swapChain;
 		std::vector<VkImage> swapChainImages;//½»»»Á´Í¼Ïñ
 		std::vector<VkImageView> swapChainImageViews;//½»»»Á´Í¼ÏñÊÓÍ¼
-
+		std::vector<VkFramebuffer> swapChainFramebuffers;//Ö¡»º³åÈÝÆ÷
 		VkFormat swapChainImageFormat;
 		VkExtent2D swapChainExtent;
 	};
